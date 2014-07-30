6810b7e8597f081945514a57d291477c8dc19914
linuxforse <linuxforse@gmail.com>
2014-07-30 13:07:11 +0300

    openrh_30-07-2014

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,11 +1,17 @@
 var http = require("http");
-function start() {
+var url = require("url");
+
+function start(router) {
 function onRequest(request,response) {
+	var pathname = url.parse(request.url).pathname;
+	console.log("Request for " + pathname + "received.");
+	router(pathname);
 response.writeHead(200,{"Content-Type": "text/plain"});
 response.write("Hello Word");
 response.end();
 }
 
 http.createServer(onRequest).listen(8888);
+console.log("Server has started.");
 }
 exports.start = start;
